792e2808e31c90c59c324e077fe6fd7ab45137e8
Alex <alex.holder@bigfootds.com>
2024-09-17 20:12:17 +1000

    feat: react app init, buttons to control number

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      numberOfPokemon: 6
+    }
+  }
+
+  exampleLog1 () {
+    console.log("Function 1 logging!");
+  }
+
+  exampleLog2 () {
+    console.log("Function 2 logging!");
+  }
+
+  decreasePokemonNumber(){
+    if (this.state.numberOfPokemon > 0){
+      this.setState({numberOfPokemon: this.state.numberOfPokemon - 1});
+    }
+  }
+
+  increasePokemonNumber(){
+    this.setState({numberOfPokemon: this.state.numberOfPokemon + 1});
+  }
+
+  render(){
+    return(
+      <>
+        <h1>Hello World!</h1>
+        <p>Blah blah blah blah </p>
+        <h2>Number of Pokemon: {this.state.numberOfPokemon}</h2>
+        <button onClick={()=> {
+          this.exampleLog1();
+          this.exampleLog2();
+          this.decreasePokemonNumber();
+        }}>
+          Decrease Pokemon Amount 
+        </button>
+        <button onClick={() => {
+          this.increasePokemonNumber();
+        }}>
+          Increase Pokemon Amount
+        </button>
+      </>
+    )
+  }
+}
+
+export default App;
